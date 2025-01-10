9f3e57879150735f4ce7d7af142c96e6f9718435
BhaveshS <bhaveshsutar001@gmail.com>
2025-01-10 16:39:24 +0530

    add frontend structure with React, Vite, Tailwind CSS, and initial pages

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import CaptainLogin from './CaptainLogin'
+
+function Home() {
+  return (
+    <div>
+
+      <div className='h-screen w-full bg-red-400 flex justify-between flex-col'>
+
+        <img src="./assets/1659761100uber-logo-png.png" alt="" className='h-10 w-10 '/>
+
+        <div className='bg-white '>
+          <h2>Get Started</h2>
+          <button>Continue</button>
+          
+        </div>
+
+      </div>
+
+    </div>
+
+  )
+}
+
+export default Home
